ddc47ef9e75b9d63c9d22676abdecb3f678ebd7d
MichaelOtieno <okellofrost@gmail.com>
2024-07-02 20:47:31 +0300

    adding sample data

--- app/src/main/java/com/example/android/readytocode/MainActivity.java
+++ app/src/main/java/com/example/android/readytocode/MainActivity.java
@@ -10,9 +10,13 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.android.readytocode.models.DataItem;
+import com.example.android.readytocode.sample.SampleDataProvider;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 TextView tvOut ;
+List<DataItem> dataItemList = SampleDataProvider.dataItemList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +30,11 @@ TextView tvOut ;
         });
 
         tvOut = findViewById(R.id.tv_out);
-        DataItem item = new DataItem(null ,"Item Name","A description",
-                "A category",1,6.78,"bishop.jpg");
-        tvOut.setText(item.toString());
+//        DataItem item = new DataItem(null ,"Item Name","A description",
+//                "A category",1,6.78,"bishop.jpg");
+        tvOut.setText("");
+        for (DataItem item: dataItemList) {
+            tvOut.append(item.getItemName()+"\n");
+        }
     }
 }
