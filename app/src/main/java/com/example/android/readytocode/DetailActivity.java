5b6017345bbf5ca41df5e18d63de32e81138f294
MichaelOtieno <okellofrost@gmail.com>
2024-07-21 02:12:37 +0300

    adding detail activity code for fetching extras

--- app/src/main/java/com/example/android/readytocode/DetailActivity.java
+++ app/src/main/java/com/example/android/readytocode/DetailActivity.java
@@ -1,6 +1,9 @@
 package com.example.android.readytocode;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -12,6 +15,12 @@ import androidx.core.view.WindowInsetsCompat;
 import com.example.android.readytocode.models.DataItem;
 import com.example.android.readytocode.sample.SampleDataProvider;
 
+import org.w3c.dom.Text;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.NumberFormat;
+import java.util.Locale;
 import java.util.Objects;
 
 public class DetailActivity extends AppCompatActivity {
@@ -37,7 +46,35 @@ public class DetailActivity extends AppCompatActivity {
         } else {
             Toast.makeText(this,"Didn't Receive any Data",Toast.LENGTH_SHORT).show();
         }
-        
+        TextView tvName = (TextView) findViewById(R.id.tvItemName);
+        TextView tvPrice = (TextView) findViewById(R.id.tvPrice);
+        TextView tvDescription = (TextView) findViewById(R.id.tvDescription);
+        ImageView  itemImage = (ImageView) findViewById(R.id.itemImage);
+
+
+        tvName.setText(item.getItemName());
+        tvDescription.setText(item.getDescription());
+
+        NumberFormat nf = NumberFormat.getCurrencyInstance(Locale.getDefault());
+        tvPrice.setText(nf.format(item.getPrice()));
+
+        InputStream inputStream = null;
+        try {
+            String imageFile = item.getImage();
+            inputStream = getAssets().open(imageFile);
+            Drawable d = Drawable.createFromStream(inputStream, null);
+            itemImage.setImageDrawable(d);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (inputStream != null) {
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
 
     }
 }
